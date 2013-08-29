4566c79ca26efe388f8bc700456fd6f852557030
Mike C. Fletcher <mcfletch@vrplumber.com>
2013-08-28 23:36:49 -0400

    Add some lables, prevent bounding-volume failures on coordinate

--- datasprite.py
+++ datasprite.py
@@ -12,6 +12,7 @@ from OpenGLContext.arrays import *
 from math import pi
 
 from OpenGLContext.scenegraph.basenodes import *
+from OpenGLContext.scenegraph import boundingvolume
 from OpenGLContext.events.timer import Timer
 
 class TestContext( BaseContext ):
@@ -20,34 +21,66 @@ class TestContext( BaseContext ):
         """Load the image on initial load of the application"""
         print """Should see a sine wave fading from green to red"""
         self.log_queue = Queue.Queue( maxsize=600 )
-        line = arange(0.0,1.0,.01)
-        line2 = line[::-1]
         self.coordinate = Coordinate(
             point = [(0,0,0)]*600,
         )
-        ymax = max(self.coordinate.point[:,1]) or 1
+        boundingvolume.cacheVolume(
+            self.coordinate,
+            boundingvolume.UnboundedVolume(),
+        )
+            
+        self.fontstyle = FontStyle(
+            family='SANS', format = 'bitmap',
+            justify = 'BEGIN',
+        )
         self.color = Color(
             color = [1.0,0.0,0.0],
         )
-        self.transform = Transform(
-                    translation = (-.5,0,0),
-                    scale = (1/3600.,1./5,0.0),
+        self.data_slider = Transform(
+            translation=(0,0,0),
+            scale = (1/1800.,1,1,),
+            children = [
+                Shape(
+                    appearance = Appearance(
+                        texture = ImageTexture( url='_particle.png' ),
+                        material = Material(
+                            diffuseColor = [1,0,0],
+                        )
+                    ),
+                    geometry = PointSet(
+                        coord = self.coordinate,
+                        minSize = 10.0,
+                        maxSize = 10.0,
+                    ),
+                ),
+            ],
+        )
+        self.axes = Transform(
+            children = [
+                Transform( 
+                    translation = (.25,coord,0),
                     children = [
-                        Shape(
-                            appearance = Appearance(
-                                texture = ImageTexture( url='_particle.png' ),
-                                material = Material(
-                                    diffuseColor = [1,0,0],
-                                )
-                            ),
-                            geometry = PointSet(
-                                coord = self.coordinate,
-                                minSize = 10.0,
-                                maxSize = 10.0,
-                            ),
-                        ),
+                        Shape( geometry = Text(
+                            string = [label],
+                            fontStyle = self.fontstyle,
+                        ))
                     ],
                 )
+                for (coord,label) in [
+                    (0,'0B'),
+                    (3,'1KB'),
+                    (6,'1MB'),
+                    (9,'1GB'),
+                ]
+            ]
+        )
+        self.transform = Transform(
+            translation = (.5,-2,0),
+            children = [
+                self.data_slider,
+                self.axes,
+            ]
+        )
         self.sg = sceneGraph(
             children = [
                 self.transform,
@@ -75,14 +108,18 @@ class TestContext( BaseContext ):
         if to_retain:
             self.coordinate.point[:to_retain] = self.coordinate.point[-to_retain:]
         self.coordinate.point[-len(new):] = new
-        self.transform.translation = (-new[0][0]/3600.) - .5,0,0
+        # trigger new-bounding-box calculation
+        boundingvolume.volumeFromCoordinate( self.coordinate )
+        self.coordinate.point = self.coordinate.point
+        self.data_slider.translation = -new[-1][0]*self.data_slider.scale[0],0,0
+        
         self.triggerRedraw()
     
 def log_reader( filename, queue ):
     """On event from the timer, generate new geometry"""
     for record in _log_reader_gen( filename ):
         queue.put( record, True )
-        time.sleep( .01 )
+        time.sleep( .005 )
 def _log_reader_gen( filename ):
     date_finder = re.compile( r'[[]\d{2}:(?P<hour>\d{2})[:](?P<minute>\d{2})[:](?P<second>\d{2})[]].*?(?P<size>[-0-9]+)$' )
     def as_size( x ):
